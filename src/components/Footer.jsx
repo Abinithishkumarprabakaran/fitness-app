19562554c2defe023f7ac35e9467c5f03903acb9
Abinithishkumarprabakaran <abinithishkumarp.a.k@gmail.com>
2023-04-22 14:22:48 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png';
 
 export default function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+    <Box mt="80px" backgroundColor="#ff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt='logo' width="200px" height="40px"/>
+        <Typography variant="h5" pb="40px" mt="20px">
+            Ak dev
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
